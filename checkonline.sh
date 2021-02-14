0af05a57cddc0d0ac25cfa595b3a177345b4ebe0
Colin Twigg <colin.f.twigg@gmail.com>
2021-02-14 11:59:11 +0000

    Update checkonline.sh

--- checkonline.sh
+++ checkonline.sh
@@ -2,8 +2,8 @@
 
 while :
 do
-    if ping -c 5 -W 1 "192.168.0.15"; then
-      cd ~/VectorSdk/ && sudo rmdir alert_run_once
+    if ping -c 5 -W 1 "<put vectors ip address here>"; then
+      cd ~/ && sudo rmdir alert_run_once
       sleep 300
     else
       python3 ./vector_alert_no_connection.py
